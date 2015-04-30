45f85a81ca3264df3458946b9fb96c991faddcca
Ben Orchard <thefirstmuffinman@gmail.com>
2015-04-30 15:25:28 +0100

    +mover, unique_id (lol)

---
+++ mover.h
@@ -0,0 +1,11 @@
+#ifndef MOVER_H_INCLUDED
+#define MOVER_H_INCLUDED
+
+#include <string>
+
+class Mover {
+    public:
+        Mover(std::string);
+};
+
+#endif // MOVER_H_INCLUDED
---
+++ unique_id.h
@@ -0,0 +1,6 @@
+#ifndef UNIQUE_ID_H_INCLUDED
+#define UNIQUE_ID_H_INCLUDED
+
+typedef int UniqueID;
+
+#endif // UNIQUE_ID_H_INCLUDED
