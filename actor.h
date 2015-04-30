dd3d21dd1f96cafe93a395fc2e99c014586be190
Ben Orchard <thefirstmuffinman@gmail.com>
2015-04-30 15:27:29 +0100

    Actor: +ID (header)

--- actor.h
+++ actor.h
@@ -1,12 +1,21 @@
-#include <string>
-
 #ifndef ACTOR_H_INCLUDED
 #define ACTOR_H_INCLUDED
 
+#include <string>
+#include "unique_id.h"
+
+// This is a base class which should NOT be instantiated alone. You
+// should use another class which inherits from this class.
+
 class Actor {
     public:
-        std::string _name;
         Actor(std::string name);
+
+        UniqueID _id;
+        std::string _name;
+
+    private:
+        UniqueID getNewID();
 };
 
 #endif // ACTOR_H_INCLUDED
