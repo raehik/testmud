6477f30d3dfbf14da4b32f5858f09f51a2ff95d6
Ben Orchard <thefirstmuffinman@gmail.com>
2015-04-30 15:26:49 +0100

    Main: print ID too

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
+#include <string>
+#include <sstream>
+
 #include "talker.h"
 
 int main() {
-    Talker character("ayy");
-    character.say("My name is " + character._name);
+    Talker actor("ayy");
+    actor.say("My name is " + actor._name);
+    int id = actor._id;
+    std::string str_id = static_cast<std::ostringstream*>( &(std::ostringstream() << id) )->str();
+    actor.say("My ID is " + str_id);
     return 0;
 }
