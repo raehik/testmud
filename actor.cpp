f88bbd52a7c3892b877b07e24cc8f81e607ee810
Ben Orchard <thefirstmuffinman@gmail.com>
2015-04-30 15:27:19 +0100

    Actor: +ID

--- actor.cpp
+++ actor.cpp
@@ -1,5 +1,15 @@
 #include "actor.h"
 
 Actor::Actor(std::string name) {
+    _id = getNewID();
     _name = name;
+}
+
+
+// private
+UniqueID Actor::getNewID() {
+    // TODO
+    // found by random die roll.
+    // guaranteed to be random.
+    return 0;
 }
