343d792e1b17e2a1acfee4c034aa1099445e1e28
Ben Orchard <thefirstmuffinman@gmail.com>
2015-05-02 18:47:58 +0100

    add WIP Position class

---
+++ position.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "area_id.hpp"
+
+class Position {
+    public:
+        Position(AreaID area, Coord x, Coord y, Coord z)
+
+    private:
+        AreaID area;
+        Coord x;
+        Coord y;
+        Coord z;
+}
