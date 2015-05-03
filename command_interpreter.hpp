379846711353126160c8b29429b2e38064657474
Ben Orchard <thefirstmuffinman@gmail.com>
2015-05-03 12:35:17 +0100

    CmdInterpreter: +split() in header

--- command_interpreter.hpp
+++ command_interpreter.hpp
@@ -1,9 +1,13 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 class CommandInterpreter {
     public:
         CommandInterpreter();
         void interpret(std::string cmd);
+
+    private:
+        std::vector<std::string> split(std::string &s, char delim);
 };
