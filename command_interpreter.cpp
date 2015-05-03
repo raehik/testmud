e5ac1e3ae3165a98bb77e1f96fa349b9ed08ff47
Ben Orchard <thefirstmuffinman@gmail.com>
2015-05-03 14:22:39 +0100

    CommandInterpreter: manual tests for a verb & noun

--- command_interpreter.cpp
+++ command_interpreter.cpp
@@ -30,6 +30,8 @@ std::vector<std::string> CommandInterpreter::split(std::string &s, char delim) {
  */
 void CommandInterpreter::interpret(std::string cmd) {
     char DELIM = ' ';
+    int VERB_INDEX = 0;
+    int ARG_START_INDEX = 1;
 
     // split command into words
     std::vector<std::string> split_cmd = split(cmd, DELIM);
@@ -44,5 +46,34 @@ void CommandInterpreter::interpret(std::string cmd) {
     // print vector items using an index
     for (std::vector<std::string>::size_type i = 0; i != split_cmd.size(); i++) {
         std::cout << "Word " << i << ": " << split_cmd[i] << std::endl;
+    }
+
+    // check if we know the verb
+    // for now, the verb is *always* the first element, but this may
+    // change later
+    std::string verb = split_cmd[VERB_INDEX];
+    if (verb == "look") {
+        // check all args *in sequence* for keywords
+        // if an unexpected word is encountered (i.e. bad grammar), end
+        // with an error
+        for (std::vector<std::string>::size_type i = ARG_START_INDEX; i != split_cmd.size(); i++) {
+            std::string word = split_cmd[i];
+
+            // do manual checks
+            if (word == "signpost") {
+                std::cout << "You look at the signpost." << std::endl;
+                break;
+            }
+
+            // allow if word is an article
+            if (word == "a" || word == "the") {
+                continue;
+            } else {
+                std::cout << "ERROR: unexpected word '" << word << "'"  << std::endl;
+                break;
+            }
+        }
+    } else {
+        std::cout << "ERROR: unknown verb '" << verb << "'" << std::endl;
     }
 }
