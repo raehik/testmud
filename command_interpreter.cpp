03831f622df68da2c738ebed4097dfe78e4a7a0e
Ben Orchard <thefirstmuffinman@gmail.com>
2015-05-03 13:38:23 +0100

    CommandInterpreter: add back reverted comments

--- command_interpreter.cpp
+++ command_interpreter.cpp
@@ -5,6 +5,29 @@
 CommandInterpreter::CommandInterpreter() {
 }
 
+// private
+/**
+ * Split a command into words (separated by spaces).
+ */
+std::vector<std::string> CommandInterpreter::split(std::string &s, char delim) {
+    // taken from http://stackoverflow.com/a/236803/2246637
+    // thanks!
+    std::vector<std::string> split_string;
+    std::stringstream ss(s);
+    std::string item;
+
+    while (std::getline(ss, item, delim)) {
+        split_string.push_back(item);
+    }
+
+    return split_string;
+}
+
+// public
+/**
+ * Interpret a command.
+ * TODO when I know how this will continue
+ */
 void CommandInterpreter::interpret(std::string cmd) {
     char DELIM = ' ';
 
@@ -22,18 +45,4 @@ void CommandInterpreter::interpret(std::string cmd) {
     for (std::vector<std::string>::size_type i = 0; i != split_cmd.size(); i++) {
         std::cout << "Word " << i << ": " << split_cmd[i] << std::endl;
     }
-}
-
-std::vector<std::string> CommandInterpreter::split(std::string &s, char delim) {
-    // taken from http://stackoverflow.com/a/236803/2246637
-    // thanks!
-    std::vector<std::string> split_string;
-    std::stringstream ss(s);
-    std::string item;
-
-    while (std::getline(ss, item, delim)) {
-        split_string.push_back(item);
-    }
-
-    return split_string;
 }
