8de349b8afbce42362937651447b6f2611499fb6
Ben Orchard <thefirstmuffinman@gmail.com>
2015-05-03 14:21:50 +0100

    CommandInterface: prompt forever

--- command_interface.cpp
+++ command_interface.cpp
@@ -8,11 +8,20 @@ CommandInterface::CommandInterface() {
 }
 
 void CommandInterface::begin() {
-    prompt();
+    while (true) {
+        prompt();
+    }
 }
 
 void CommandInterface::prompt() {
+    // print a prompt
     std::cout << _prompt_str;
+
+    // get & store a line from the user
     std::getline(std::cin, _input_cmd);
+
+    // interpret command
+    // TODO: should this execute too? interface shouldn't say to execute
+    // after interpreting, right?
     _cmd_interpreter.interpret(_input_cmd);
 }
